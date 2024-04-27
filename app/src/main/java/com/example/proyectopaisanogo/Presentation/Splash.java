a829f9e2daf8df6e056657a93c8da1a13d87cb7a
GabrielaPrietoH <123327970+GabrielaPrietoH@users.noreply.github.com>
2024-04-27 11:04:46 +0100

    add splash

--- app/src/main/java/com/example/proyectopaisanogo/Presentation/Splash.java
+++ app/src/main/java/com/example/proyectopaisanogo/Presentation/Splash.java
@@ -1,8 +1,18 @@
 package com.example.proyectopaisanogo.Presentation;
 
+import static com.example.proyectopaisanogo.R.color.colorPrimaryDark;
+
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,28 +23,49 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.proyectopaisanogo.R;
 
 public class Splash extends AppCompatActivity {
+    public ProgressBar splash_screenProgressBar;
+    public int MAX_VALUE = 30;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.modern_proyect);
+         mediaPlayer.start();
+
+        // Establece la orientación vertical
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        ImageView txt = (ImageView) findViewById(R.id.imageView2);
+        Animation aniSlide = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim1);
+        txt.startAnimation(aniSlide);
+        splash_screenProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        splash_screenProgressBar.setMax(MAX_VALUE);
 
-        int tiempo= 2000; // 2 segundos en milisegundos
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        // Agrega el código para actualizar la barra de progreso aquí
+        new CountDownTimer(5000, 100) {
+            int progreso = 1;
+
             @Override
-            public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            public void onTick(long millisUntilFinished) {
+                splash_screenProgressBar.setProgress(progreso);
+                progreso += (1);
             }
-        }, tiempo);
+
+            @Override
+            public void onFinish() {
+                splash_screenProgressBar.setProgress(MAX_VALUE);
+
+                // Inicia la siguiente actividad
+                Intent mainIntent = new Intent().setClass(Splash.this, MainActivity.class);
+                startActivity(mainIntent);
+                // Cierra la actividad para que el usuario no pueda volver a esta actividad presionando el botón Atrás
+                finish();
+            }
+
+        }.start();
+
 
     }
 }
